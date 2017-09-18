a2f318f25a724010ab0bcb9e5043925d1a827824
John Boyes <john@edinburghwebservices.co.uk>
2017-09-18 11:55:52 +0100

    Use HTTP POST receiving JSON returning CSV

---
+++ lib/jira_cycle_time_api.rb
@@ -0,0 +1,28 @@
+require 'grape'
+require_relative 'jira_cycle_time'
+
+module JIRAControlChartData
+  # API allowing raw JIRA control chart data to be received and translated into a CSV with the cycle times
+  class API < Grape::API
+    version 'v1', using: :header, vendor: 'jira-control-chart-data'
+    format :json
+    prefix :api
+
+    helpers do
+      def raw_control_chart_json(params)
+        params.select { |k, _v| %w(issues columns currentTime workRateData).include? k }
+      end
+    end
+
+    resource :cycle_time do
+      desc 'Cycle times in CSV format'
+      post do
+        status 200
+        header 'Content-Disposition', 'attachment; filename=jira_cycle_time.csv'
+        content_type 'application/csv'
+        env['api.format'] = 'application/csv'
+        JiraCycleTime.new(raw_control_chart_json(params), params[:in_progress_columns]).as_csv
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,55 +1,24 @@
 require 'simplecov'
 SimpleCov.start
-require 'dotenv'
-Dotenv.load('spec/env/test.env')
-require 'rake'
+require 'rspec'
+require 'capybara/rspec'
+require_relative '../lib/jira_cycle_time_api'
 
-def copy_with_path(source, destination)
-  FileUtils.mkdir_p(File.dirname(destination))
-  FileUtils.cp(source, destination)
-end
-
-def delete_input_and_output_directory
-  FileUtils.rm_rf(INPUT_AND_OUTPUT_DIRECTORY)
-end
-
-# fix for RSpec/Rake bug - see https://github.com/rspec/rspec-core/issues/2314#issuecomment-241253384
-module Rake
-  class Application
-    def handle_options
-      options.rakelib = ['rakelib']
-      options.trace_output = $stderr
-      option_parser
-    end
-
-    def option_parser
-      OptionParser.new do |opts|
-        opts.banner = "#{Rake.application.name} [-f rakefile] {options} targets..."
-        opts.separator ''
-        opts.separator 'Options are ...'
-        print_on_tail(opts)
-        standard_rake_options.each { |args| opts.on(*args) }
-        opts.environment('RAKEOPT')
-      end.parse(sanitise_command_line_arguments(ARGV))
-    end
-
-    def print_on_tail(opts)
-      opts.on_tail('-h', '--help', '-H', 'Display this help message.') do
-        puts opts
-        exit
-      end
-    end
-
-    # This is the fix - we don't want the command line arguments to be parsed when running Rake in RSpec
-    def sanitise_command_line_arguments(_args)
-      []
-    end
-  end
+def app
+  JIRAControlChartData::API
 end
 
 RSpec.configure do |config|
-  config.before(:suite) do
-    Rake.application.init
-    Rake.application.load_rakefile
+  config.include Rack::Test::Methods
+  config.before :each do
   end
+end
+
+def assert_last_csv_response(expected_body, expected_status = 200)
+  expect(last_response.status).to eq expected_status
+  expect(CSV.parse(last_response.body)).to eq expected_body
+end
+
+def post_json(url, json)
+  post(url, json, 'CONTENT_TYPE' => 'application/json')
 end
