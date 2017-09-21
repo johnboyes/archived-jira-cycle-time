7e3621606d83a410af44ae3d9a6f2d700f4c7131
John Boyes <john@edinburghwebservices.co.uk>
2017-09-21 11:10:49 +0100

    Exclude reopened issues which are in progress

--- spec/jira_cycle_time_spec.rb
+++ spec/jira_cycle_time_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-feature 'JIRA cycle times CSV export' do
-  scenario 'valid request with raw control chart data' do
+feature 'JIRA cycle times CSV export' do # rubocop:disable Metrics/BlockLength
+  scenario 'valid raw control chart data' do
     expected = [
       ['Completed Date', 'Start Date', 'Type', 'Id', 'Description'],
       ['03/09/2017', '03/09/2017', '', 'AUTOMATED-14', 'Sunday afternoon issue'],
@@ -12,12 +12,22 @@ feature 'JIRA cycle times CSV export' do
     assert_last_csv_response expected
   end
 
-  scenario 'valid request with data that has issues that are still in progress' do
+  scenario 'issues that are still in progress are excluded' do
     expected = [
       ['Completed Date', 'Start Date', 'Type', 'Id', 'Description'],
       ['03/09/2017', '03/09/2017', '', 'AUTOMATED-13', 'Sunday morning issue']
     ]
     json = File.read('example_raw_jira_data_with_in_progress_issue.json')
+    post_json('/api/cycle_time?in_progress_columns[]=In+Progress&done_column=Done', json)
+    assert_last_csv_response expected
+  end
+
+  scenario 'reopened issues that are still in progress are excluded' do
+    expected = [
+      ['Completed Date', 'Start Date', 'Type', 'Id', 'Description'],
+      ['03/09/2017', '03/09/2017', '', 'AUTOMATED-13', 'Sunday morning issue']
+    ]
+    json = File.read('example_raw_jira_data_with_reopened_in_progress_issue.json')
     post_json('/api/cycle_time?in_progress_columns[]=In+Progress&done_column=Done', json)
     assert_last_csv_response expected
   end
